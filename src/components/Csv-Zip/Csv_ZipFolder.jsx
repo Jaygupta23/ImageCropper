83c7e2ee99e034682a42d5c99400557a13a89c77
JAY GUPTA <jaygupta5474@gmail.com>
2024-04-02 10:51:13 +0530

    add save file button

--- src/components/Csv-Zip/Csv_ZipFolder.jsx
+++ src/components/Csv-Zip/Csv_ZipFolder.jsx
@@ -3,21 +3,20 @@ import UploadFile from "../../assets/photo.png";
 const Csv_ZipFolder = () => {
   return (
     <>
-      <div
-        className="flex justify-center gap-5 bg-gray-00 h-[90vh] my-5 py-5"
-      >
+      <div className="flex justify-center gap-5 h-[90vh] my-5 pt-3">
         <div className="me-5 max-w-xl border-4 border-dashed  border-indigo-400 h-[50%] w-[25%] rounded-3xl p-8 text-center shadow-lg">
           <img
             src={UploadFile}
             alt="uploadIcon"
-            className="w-[25%] mx-auto mt-5 mb-4"
+            width={"25%"}
+            className=" mx-auto mt-5 pt-5 mb-4"
           />
-          <h2 className=" text-xl font-semibold text-gray-500 mb-4">
+          <h2 className=" text-xl font-semibold text-gray-500 mb-4 mt-5">
             Drag and Drop file to upload <br /> or{" "}
           </h2>
           <div className="relative flex justify-center">
             <label
-              className="flex items-center font-medium text-white bg-gradient-to-r from-purple-600 to-indigo-700 rounded-lg shadow-md cursor-pointer select-none text-lg px-6 py-3 hover:shadow-xl active:shadow-md"
+              className="flex items-center font-medium text-white bg-gradient-to-r from-purple-600 to-indigo-700 rounded-3xl shadow-md cursor-pointer select-none text-lg px-6 py-2 hover:shadow-xl active:shadow-md"
               htmlFor="file-upload"
             >
               <span>Upload CSV File</span>
@@ -39,15 +38,16 @@ const Csv_ZipFolder = () => {
           <img
             src={UploadFile}
             alt="uploadIcon"
-            className="w-[25%] mx-auto mt-5 mb-4"
+            width={"25%"}
+            className=" mx-auto mt-5 pt-5 mb-4"
           />
 
-          <h2 className=" text-xl font-semibold text-gray-500 mb-4">
+          <h2 className=" text-xl font-semibold text-gray-500 mb-4 mt-5">
             Drag and Drop file to upload <br /> or{" "}
           </h2>
           <div className="relative flex justify-center">
             <label
-              className="flex items-center font-medium text-white bg-gradient-to-r from-purple-600 to-indigo-700 rounded-lg shadow-md cursor-pointer select-none text-lg px-6 py-3 hover:shadow-xl active:shadow-md"
+              className="flex items-center font-medium text-white bg-gradient-to-r from-purple-600 to-indigo-700 rounded-3xl shadow-md cursor-pointer select-none text-lg px-6 py-2 hover:shadow-xl active:shadow-md"
               htmlFor="file-upload"
             >
               <span>Upload Zip file </span>
@@ -60,12 +60,12 @@ const Csv_ZipFolder = () => {
             />
           </div>
           <p className="text-gray-500 font-medium my-3">
-            Supported files: xlsx
+            Supported files: .zip
           </p>
         </div>
       </div>
-      <div className="flex justify-center mb-5">
-        <button className="bg-indigo-700 text-white px-4 py-2 text-xl font-medium rounded-xl">Save Files </button>
+      <div className="flex justify-center bg-indigo-400 pt-2 mb-4">
+        <button type="submit" className="btn btn-lg btn-success text-gray px-4 py-2 text-xl font-medium rounded-3xl">Save Files </button>       
       </div>
     </>
   );
